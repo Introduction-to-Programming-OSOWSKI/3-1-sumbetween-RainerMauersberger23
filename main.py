0bbec4e4517099cfbe38f4097ea1287565edb5d5
RainerMauersberger23 <829532@mystma.org>
2021-01-19 20:53:23 +0000

    Loops

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-#WRITE YOUR CODE IN THIS FILE
+def sumBetween(x, y):
+   
+    cat = 0
+   
+    for i in range (x + 1, y):
+        cat = cat + i
+        
+    return cat
+
+print (sumBetween(5,7))
